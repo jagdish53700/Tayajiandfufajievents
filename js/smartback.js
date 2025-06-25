4b4d2510008e4e72e2b1dc414bdb2fbd31abf852
jagdish53700 <125004304+jagdish53700@users.noreply.github.com>
2025-06-25 15:07:18 +0530

    Delete js/smartback.js

--- js/smartback.js
+++
@@ -1,8 +0,0 @@
- document.addEventListener("DOMContentLoaded", function () {
-    const params = new URLSearchParams(window.location.search);
-    if (params.get("reopenmenu") === "true") {
-      const toggle = document.getElementById("menu-toggle");
-      if (toggle) toggle.checked = true;
-      history.replaceState(null, "", window.location.pathname); // Clean up URL
-    }
-  });
